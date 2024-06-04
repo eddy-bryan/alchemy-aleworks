6663d5197e8d0fb0d3549c294c0aeda907185294
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-06-04 17:55:54 +0000

    Added admin classes to customise the admin panel display

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Beer, MerchItem
 
 
-admin.site.register(Beer)
-admin.site.register(MerchItem)
+class BeerAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'type',
+        'alcohol_content',
+        'price',
+        'limited_edition',
+        'image',
+    )
+    
+    ordering = ('sku',)
+
+class MerchItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'sized_item',
+        'image',
+    )
+    
+    ordering = ('sku',)
+
+admin.site.register(Beer, BeerAdmin)
+admin.site.register(MerchItem, MerchItemAdmin)
