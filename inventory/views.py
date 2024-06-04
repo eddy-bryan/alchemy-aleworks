5ea58165d9f59dcc72810635d50488ef90244ca7
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-06-04 18:21:55 +0000

    Created and linked up beers template to the beer model

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Beer
 
-# Create your views here.
+def beers(request):
+    """ A view to show, filter and sort all beers. """
+    beers = Beer.objects.all()
+    context = {
+        'beers': beers,
+    }
+    return render(request, 'inventory/beers.html', context)
