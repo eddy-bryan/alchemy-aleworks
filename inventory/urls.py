99b892eccaef8f6ff814c8bea38951bb00a8eb26
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-06-05 18:57:21 +0000

    Created and linked up merch view

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('beers/', views.beers, name='beers'),
+    path('merch/', views.merch, name='merch'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Beer
+from .models import Beer, MerchItem
 
 def beers(request):
     """ A view to show, filter and sort all beers. """
@@ -8,3 +8,11 @@ def beers(request):
         'beers': beers,
     }
     return render(request, 'inventory/beers.html', context)
+
+def merch(request):
+    """ A view to show, filter and sort all beers. """
+    merch = MerchItem.objects.all()
+    context = {
+        'merch': merch,
+    }
+    return render(request, 'inventory/merch.html', context)
