53233cb64c4daf7d8fff125f55d1af6b1242347d
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-06-25 19:20:39 +0000

    Added sorting funtionality for beers

--- inventory/views.py
+++ inventory/views.py
@@ -7,7 +7,8 @@ def beers(request):
     """ A view to show, filter and sort all beers. """
     limited_edition = request.GET.get('limited_edition', '').lower() == 'true'
     beers = Beer.objects.all()
-    
+
+    # Filter based on limited edition
     if limited_edition:
         beers = beers.filter(limited_edition=True)
         title = "Limited Edition Beers"
@@ -17,13 +18,29 @@ def beers(request):
     else:
         title = "All Beers"
 
+    # Sorting functionality
+    sort_by = request.GET.get('sort_by')
+    if sort_by == 'price_asc':
+        beers = beers.order_by('price')
+    elif sort_by == 'price_desc':
+        beers = beers.order_by('-price')
+    elif sort_by == 'name_asc':
+        beers = beers.order_by('name')
+    elif sort_by == 'name_desc':
+        beers = beers.order_by('-name')
+    elif sort_by == 'alcohol_asc':
+        beers = beers.order_by('alcohol_content')
+    elif sort_by == 'alcohol_desc':
+        beers = beers.order_by('-alcohol_content')
+
+    # Get random beers
     limited_edition_beer = Beer.get_random_beers(limited_edition=True, quantity=1)
     core_range_beer = Beer.get_random_beers(limited_edition=False, quantity=1)
     context = {
         'beers': beers,
         'limited_edition_beer': limited_edition_beer[0] if limited_edition_beer else None,
         'core_range_beer': core_range_beer[0] if core_range_beer else None,
-        'title': title,  # Pass the title to the template
+        'title': title,
     }
     return render(request, 'inventory/beers.html', context)
 
@@ -31,7 +48,8 @@ def merch(request):
     """ A view to show, filter and sort all merch items. """
     category = request.GET.get('category', '').lower()
     merch = MerchItem.objects.all()
-    
+
+    # Filter based on category
     if category == 'drinkware':
         merch = merch.filter(category__iexact='Drinkware')
         title = "Drinkware"
@@ -41,13 +59,25 @@ def merch(request):
     else:
         title = "All Merch"
 
+    # Sorting functionality
+    sort_by = request.GET.get('sort_by')
+    if sort_by == 'price_asc':
+        merch = merch.order_by('price')
+    elif sort_by == 'price_desc':
+        merch = merch.order_by('-price')
+    elif sort_by == 'name_asc':
+        merch = merch.order_by('name')
+    elif sort_by == 'name_desc':
+        merch = merch.order_by('-name')
+
+    # Get random merch items
     random_apparel = MerchItem.get_random_merch_items(category="Apparel & Accessories", quantity=1)
     random_drinkware = MerchItem.get_random_merch_items(category="Drinkware", quantity=1)
     context = {
         'merch': merch,
         'random_apparel': random_apparel[0] if random_apparel else None,
         'random_drinkware': random_drinkware[0] if random_drinkware else None,
-        'title': title,  # Pass the title to the template
+        'title': title,
     }
     return render(request, 'inventory/merch.html', context)
 
