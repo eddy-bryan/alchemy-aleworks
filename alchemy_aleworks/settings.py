31463bc1fe27f74715826fbb3b2d3bbf4a533de0
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-07-15 19:08:29 +0000

    Adjusted AWS settings and prepared for Heroku re-deployment

--- alchemy_aleworks/settings.py
+++ alchemy_aleworks/settings.py
@@ -210,7 +210,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
-    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
